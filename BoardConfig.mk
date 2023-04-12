f02315f7437e77867a52bdf412f507d759049dec
FlowerSea0208 <zzp060208@gmail.com>
2023-04-12 12:18:18 +0800

    renoir: Decommonize screen density
    Change-Id: I5594d0d85d0c766c2af2eac93e8f63daf5cf9e1e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := renoir
 
+# Display
+TARGET_SCREEN_DENSITY := 440
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/hidl/manifest.xml
 
